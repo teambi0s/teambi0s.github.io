c3514918ebb5455a9b65bd9832b8dc2afd3c8f9f
Ashwin Shenoy <aswinshenoy65@gmail.com>
2022-07-31 22:12:26 +0530

    responsive fix

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => (
     <footer className="flex flex-wrap py-8">
-        <div className="w-100 md:w-1/2 flex items-center text-center md:text-left p-2 text-sm opacity-80">
+        <div className="w-100 md:w-1/2 flex items-center justify-center md:justify-start p-2 text-sm opacity-80">
             &copy; Team bi0s 2007 - {new Date().getFullYear()}. All rights reserved.
         </div>
         <div className="w-100 md:w-1/2 p-2 flex justify-center md:justify-end items-center">
